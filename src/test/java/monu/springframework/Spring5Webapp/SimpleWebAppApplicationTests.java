d5c2ce1a6af5ef71385f7e9513864ec399c45284
Monu <ecemonu@gmail.com>
2020-08-14 16:35:46 +0530

    first commit

---
+++ src/test/java/monu/springframework/Spring5Webapp/SimpleWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package monu.springframework.Spring5Webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
